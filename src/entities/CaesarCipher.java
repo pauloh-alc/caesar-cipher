7d288b4f29f4b1489725bbf0fe9db76af4fe63c1
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-09-09 20:37:50 -0300

    Criando método para realizar criptografia
    Realizando criação de método nomeado encriptUsingCaesarCipher que
    realiza uma criptografia de determinada entrada de texto, fazendo uso da
    Cifra de Caesar.

--- src/entities/CaesarCipher.java
+++ src/entities/CaesarCipher.java
@@ -1,18 +1,39 @@
 package entities;
 
 public class CaesarCipher {
-	
+
 	static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	
-	public static final String generateShiftedAlphabet(int key) {
-		String slice = ALPHABET.substring(key); 
-		String shiftedAlphabet = slice + ALPHABET.substring(0, key);
+
+	public static String encriptUsingCaesarCipher(String input, int key) {
+
+		String shiftedAlphabet = CaesarCipher.generateShiftedAlphabet(key);
+		StringBuilder encrypted = new StringBuilder(input.toUpperCase());
 		
+		for (int i = 0; i < encrypted.length(); i++) {
+			char ch = encrypted.charAt(i);
+			int idx = ALPHABET.indexOf(ch);
+			if (idx != -1) {
+				encrypted.setCharAt(i, shiftedAlphabet.charAt(idx));
+			}
+		}
+		return encrypted.toString();
+	}
+
+	public static final String generateShiftedAlphabet(int key) {
+		String shiftedAlphabet = ALPHABET.substring(key) + ALPHABET.substring(0, key);
 		return shiftedAlphabet;
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println(ALPHABET);
-		System.out.println(generateShiftedAlphabet(23));
+		int key = 19;
+
+		System.out.println("Alphabet........: " + ALPHABET);
+		System.out.println("Shifted Alphabet: " + generateShiftedAlphabet(key));
+		
+		System.out.println();
+		
+		String text = "Paulo Alencar vai para a guerra com Julio Cesar";
+		System.out.println("Text.....: " + text);
+		System.out.println("Encrypted: " + encriptUsingCaesarCipher(text, key));
 	}
 }
