4ee3230af2ddf109956cb11d164da538789009b8
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-09-09 18:41:49 -0300

    Adicionando alfabeto e criando alfabeto deslocado
    Adicionando alfabeto [A-Z] e realizando criação de alfabeto deslocado,
    com base em uma chave.

--- src/entities/CaesarCipher.java
+++ src/entities/CaesarCipher.java
@@ -1,8 +1,22 @@
 package entities;
 
+import java.util.Scanner;
+
 public class CaesarCipher {
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		
+		System.out.print("key: ");
+		Integer key = sc.nextInt();
+		
+		String slice = alphabet.substring(key); 
+		String shiftedAlphabet = slice + alphabet.substring(0, key);
+		
+		System.out.println("Alphabet........: " + alphabet);
+		System.out.println("Shifted Alphabet: " + shiftedAlphabet);
+		
+		sc.close();
 	}
 }
