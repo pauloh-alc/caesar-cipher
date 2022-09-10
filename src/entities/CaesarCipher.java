012dece08cfa492d7021d86950b9ae5ef42056ab
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-09-09 23:05:51 -0300

    Criando sobrecarga para método de criptografia
    Utilizando sobrecarga para método encriptUsingCaesarCipher. O método é
    composto por 3 parâmetros, uma String chamada de input e dois inteiros
    chamados de key1 e key2. O key1 é usado para criptografar todos os
    caracteres de índices pares e o parâmetro key2 vai criptografar os
    caracteres de índices ímpares.

--- src/entities/CaesarCipher.java
+++ src/entities/CaesarCipher.java
@@ -28,22 +28,59 @@ public class CaesarCipher {
 		}
 		return encrypted.toString();
 	}
+	
+	public static String encriptUsingCaesarCipher(String input, int key1, int key2) {
 
+		String shiftedAlphabet1 = CaesarCipher.generateShiftedAlphabet(key1);
+		String shiftedAlphabet2 = CaesarCipher.generateShiftedAlphabet(key2);
+		StringBuilder encrypted = new StringBuilder(input);
+		String shiftedAlphabet;
+		
+		for (int i = 0; i < encrypted.length(); i++) {
+			char ch = encrypted.charAt(i);
+			int idx;
+			
+			if (i % 2 == 0) 
+				shiftedAlphabet = shiftedAlphabet1;
+			else 
+				shiftedAlphabet = shiftedAlphabet2;
+			
+			if (Character.isLowerCase(ch)) {
+				shiftedAlphabet = shiftedAlphabet.toLowerCase();
+				idx = ALPHABET.toLowerCase().indexOf(ch);
+			}
+			else { 
+				shiftedAlphabet = shiftedAlphabet.toUpperCase();
+				idx = ALPHABET.indexOf(ch);
+			}
+			
+			if (idx != -1) {
+				encrypted.setCharAt(i, shiftedAlphabet.charAt(idx));
+			}
+		}
+		return encrypted.toString();
+	}
+	
 	public static final String generateShiftedAlphabet(int key) {
 		String shiftedAlphabet = ALPHABET.substring(key) + ALPHABET.substring(0, key);
 		return shiftedAlphabet;
 	}
 
 	public static void main(String[] args) {
-		int key = 3;
+		int key1 = 23;
+		int key2 = 17;
 
-		System.out.println("Alphabet........: " + ALPHABET);
-		System.out.println("Shifted Alphabet: " + generateShiftedAlphabet(key));
+		System.out.println("Alphabet...............: " + ALPHABET);
+		System.out.println("Shifted Alphabet (key1): " + generateShiftedAlphabet(key1));
+		System.out.println("----------------------------------");
+		System.out.println("Alphabet...............: " + ALPHABET);
+		System.out.println("Shifted Alphabet (key2): " + generateShiftedAlphabet(key2));
 		
 		System.out.println();
 		
-		String text = "Paulo Henrique vai para guerra";
-		System.out.println("Text.....: " + text);
-		System.out.println("Encrypted: " + encriptUsingCaesarCipher(text, key));
+		String text = "First Legion";
+		System.out.println("Text............: " + text);
+		System.out.println("Encrypted (key1): " + encriptUsingCaesarCipher(text, key1));
+		System.out.println("Encrypted (key1 e key2): " + encriptUsingCaesarCipher(text, key1, key2));
 	}
 }
