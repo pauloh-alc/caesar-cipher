6ca0a35258fd931163951af003ec835f81cbd1b8
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-09-10 23:14:12 -0300

    Criando novo método humanDecrypt
    Realizando a criação do método humanDecrypt, reponsável por
    descriptgrafar a mensagem codificada através do método "eye ball decrypt".
    Além disso, decidi criar um novo pacote nomeado application, nesse
    pacote criei uma classe para realização de testes, nomeada Test.
    
    Refatorei os métodos encriptUsingCaesarCipher, então eles deixaram de
    ser métodos estáticos. Isso me forçou a sempre criar uma instância de
    CaesarCipher na classe Test quando estava realizando meus testes.
    
    Por fim, realizei mudança no modificador de acesso do atributo ALPHABET,
    coloquei como private. Com isso, surgiu a necessidade de criar um getter
    para esse atributo.

--- src/entities/CaesarCipher.java
+++ src/entities/CaesarCipher.java
@@ -2,11 +2,22 @@ package entities;
 
 public class CaesarCipher {
 
-	static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	public CaesarCipher() {
+	
+	}
+	
+	public String getALPHABET() {
+		return ALPHABET;
+	}
+	
+	/*
+	 * Implementing the Caesar Cipher
+	 */
+	public String encriptUsingCaesarCipher(String input, int key) {
 
-	public static String encriptUsingCaesarCipher(String input, int key) {
-
-		String shiftedAlphabet = CaesarCipher.generateShiftedAlphabet(key);
+		String shiftedAlphabet = generateShiftedAlphabet(key);
 		StringBuilder encrypted = new StringBuilder(input);
 		
 		for (int i = 0; i < encrypted.length(); i++) {
@@ -29,10 +40,10 @@ public class CaesarCipher {
 		return encrypted.toString();
 	}
 	
-	public static String encriptUsingCaesarCipher(String input, int key1, int key2) {
+	public String encriptUsingCaesarCipher(String input, int key1, int key2) {
 
-		String shiftedAlphabet1 = CaesarCipher.generateShiftedAlphabet(key1);
-		String shiftedAlphabet2 = CaesarCipher.generateShiftedAlphabet(key2);
+		String shiftedAlphabet1 = generateShiftedAlphabet(key1);
+		String shiftedAlphabet2 = generateShiftedAlphabet(key2);
 		StringBuilder encrypted = new StringBuilder(input);
 		String shiftedAlphabet;
 		
@@ -61,33 +72,21 @@ public class CaesarCipher {
 		return encrypted.toString();
 	}
 	
-	public static final String generateShiftedAlphabet(int key) {
+	public final String generateShiftedAlphabet(int key) {
 		String shiftedAlphabet = ALPHABET.substring(key) + ALPHABET.substring(0, key);
 		return shiftedAlphabet;
 	}
-
-	public static void main(String[] args) {
-		int key1 = 23;
-		int key2 = 17;
-
-		System.out.println("Alphabet...............: " + ALPHABET);
-		System.out.println("Shifted Alphabet (key1): " + generateShiftedAlphabet(key1));
-		System.out.println("----------------------------------");
-		System.out.println("Alphabet...............: " + ALPHABET);
-		System.out.println("Shifted Alphabet (key2): " + generateShiftedAlphabet(key2));
-		
-		System.out.println();
-		
-		String text = "First Legion";
-		System.out.println("Text............: " + text);
-		System.out.println("Encrypted (key1): " + encriptUsingCaesarCipher(text, key1));
-		System.out.println("Encrypted (key1 e key2): " + encriptUsingCaesarCipher(text, key1, key2));
-		
-		System.out.println("=====================================================");
-		String path = "/tmp/file";
-		FileResource fr = new FileResource(path);
-		String message = fr.asString();
-		String encrypted = encriptUsingCaesarCipher(message, key1);
-		System.out.println("key is " + key1 + "\n" + encrypted);
+	
+	/*
+	 * Breaking the Caesar Cipher
+	 */
+	
+	// Abordagem - Human Decryption
+	
+	public void humanDecrypt(String encrypted) {
+		for (int key = 0; key < ALPHABET.length(); key++) {
+			String output = this.encriptUsingCaesarCipher(encrypted, key);
+			System.out.println("key " + key + ": " + output);
+		}
 	}
 }
